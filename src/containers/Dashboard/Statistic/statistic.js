b49473de8e2f55b8f9070bbfc813b75b249ab633
Cuong Le <cuongle.devplus@gmail.com>
2022-04-26 16:57:59 +0700

    FE_9

--- src/containers/Dashboard/Statistic/statistic.js
+++ src/containers/Dashboard/Statistic/statistic.js
@@ -2,49 +2,6 @@ import React from "react";
 import "./statistic.css";
 import { Col, Row, Divider, Tag, Progress, Statistic } from "antd";
 import { ArrowUpOutlined } from "@ant-design/icons";
-import {
-    LineChart,
-    ResponsiveContainer,
-    Legend, Tooltip,
-    Line,
-    XAxis,
-    YAxis,
-    CartesianGrid
-} from 'recharts'
-
-const pdata = [
-    {
-        name: 'MongoDb',
-        student: 11,
-        fees: 120
-    },
-    {
-        name: 'Javascript',
-        student: 15,
-        fees: 12
-    },
-    {
-        name: 'PHP',
-        student: 5,
-        fees: 10
-    },
-    {
-        name: 'Java',
-        student: 10,
-        fees: 5
-    },
-    {
-        name: 'C#',
-        student: 9,
-        fees: 4
-    },
-    {
-        name: 'C++',
-        student: 10,
-        fees: 8
-    },
-];
-
 const TopCard = ({ title, tagContent, tagColor, prefix }) => {
   return (
     <Col className="gutter-row" span={6}>
